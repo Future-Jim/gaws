a11419d623c12a179778fc4959ca14ca06e1d489
James Arias <jamesarias@protonmail.com>
2023-06-08 22:01:34 -0400

    refactoring cmds

--- cmd/gaws/directoryupload.go
+++ cmd/gaws/directoryupload.go
@@ -1,29 +1,29 @@
 package gaws
 
 import (
-	
-	"os"
-	"fmt"
 	"errors"
-	"github.com/spf13/cobra"
-	"github.com/manifoldco/promptui"
-	"github.com/future-jim/gaws/pkg/gaws"
+	"fmt"
 
+	"os"
+
+	"github.com/future-jim/gaws/pkg/gaws"
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
 )
 
 var diruploadCmd = &cobra.Command{
-    Use:   "directory-upload",
-    Aliases: []string{"directory-upload"},
-    Short:  "uploads a directory as a tar archive to a specified S3 bucket",
-    Args:  cobra.ExactArgs(0),
-    Run: func(cmd *cobra.Command, args []string) {
-        archiveUpload()
-        
-    },
+	Use:     "directory-upload",
+	Aliases: []string{"du"},
+	Short:   "uploads a directory as a tar archive to a specified S3 bucket",
+	Args:    cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		archiveUpload()
+
+	},
 }
 
 func init() {
-    rootCmd.AddCommand(diruploadCmd)
+	rootCmd.AddCommand(diruploadCmd)
 }
 
 type promptContent struct {
@@ -37,7 +37,7 @@ func archiveUpload() {
 		"Enter bucket to upload archive to",
 	}
 
-	buckets, err := gaws.BucketList()
+	buckets, err := gaws.ListBuckets()
 	if err != nil {
 		fmt.Printf("Couldnt list buckets: %v", err)
 	}
@@ -50,13 +50,13 @@ func archiveUpload() {
 		"Please enter a local directory",
 		"Enter the relative path of directory to archive and upload",
 	}
-	
+
 	directory := promptGetInput(directoryPromptContent)
 	fmt.Printf("The directory you selected is %s\n\n", directory)
-	
+
 	filenamePromptContent := promptContent{
-		"Please enter a filename for the archive",
-		"Enter filename for archive:",
+		"Please enter a filename for the archive without a file extension",
+		"Enter filename for archive without a file extension:",
 	}
 
 	filename := promptGetInput(filenamePromptContent)
@@ -65,18 +65,17 @@ func archiveUpload() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	
-	file := gaws.CreateTar(filename, directory)
 
+	//	file := gaws.CreateTar(filename, directory)
+	file := gaws.ArchiveDir(filename, directory)
 	for _, buckets := range buckets.Buckets {
-		   if bucket == *buckets.Name {
-			gaws.S3Fileupload(filename, file, bucket)			
+		if bucket == *buckets.Name {
+			gaws.S3Fileupload(filename, file, bucket)
 			return
-		}		
+		}
 	}
 	gaws.CreateBucket(bucket)
-	gaws.S3Fileupload(filename, file, bucket)
-	return 
+	gaws.S3Fileupload(filename+".zip", file, bucket)
 }
 
 func promptGetInput(pc promptContent) string {
@@ -102,7 +101,7 @@ func promptGetInput(pc promptContent) string {
 
 	result, err := prompt.Run()
 	if err != nil {
-                                    		fmt.Printf("Prompt failed %v\n", err)
+		fmt.Printf("Prompt failed %v\n", err)
 		os.Exit(1)
 	}
 
--- go.mod
+++ go.mod
@@ -1,22 +1,18 @@
-module github.com/future-jim/gaws
+module gaws
 
 go 1.20
 
 require (
-	github.com/aws/aws-sdk-go v1.44.277
+	github.com/aws/aws-sdk-go v1.44.279
+	github.com/future-jim/gaws v0.0.0-20230608151527-08d3cf7de406
 	github.com/manifoldco/promptui v0.9.0
 	github.com/spf13/cobra v1.7.0
 )
 
 require (
-	9fans.net/go v0.0.0-20181112161441-237454027057 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/mdempsky/gocode v0.0.0-20200405233807-4acdcbdea79d // indirect
-	github.com/rogpeppe/godef v1.1.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
 )
